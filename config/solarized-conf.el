204b38d0297d89c9296e86d7f22691cda6c0f534
Alexander Goldberg <algold@gmail.com>
2013-08-12 15:45:17 +0300

    Fix bloody trash with yellow git gutter

--- config/solarized-conf.el
+++ config/solarized-conf.el
@@ -3,3 +3,10 @@
 (live-add-pack-lib "solarized-emacs")
 (require 'solarized)
 (load-theme 'solarized-dark t)
+
+
+
+;; Face fixes from original themes, that were not pulled into respective repos
+
+;; Fix bloody trash with yellow git-gutter
+(set-face-background 'git-gutter:unchanged nil)')
